7787c1741e810587f73dba85b039a4c15bef4476
lzl1024 <zhuolinliu8@gmail.com>
2013-12-04 00:40:12 -0500

    constant change

--- src/constant/Constants.java
+++ src/constant/Constants.java
@@ -5,14 +5,12 @@ public class Constants {
     public static double difference = 1;
     // number of cluster
     public static int K = 10;
-    //public static int NUM_OF_POINTS = 20;
    
     // points 2D
     public static double pointRealm = 1000;
     // the max euclidean distance between centroid
     public static int pointsInCluster = 50000;
     public static double MIN_POINT_DIS = pointRealm / Constants.K;
-    public static final double STDIVATION = 1;
     public static double CLUSTER_REALM = MIN_POINT_DIS / 2;
 
     // DNA
